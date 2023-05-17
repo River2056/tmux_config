025b7efeebbe095f6a1842d7bbb7b980bb237444
river2056 <chen0625tung@gmail.com>
2023-05-17 22:45:02 +0800

    + added auto include tpm package

--- symlink.sh
+++ symlink.sh
@@ -1,2 +1,4 @@
+[ ! -d ~/.tmux/plugins/tpm ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 [ ! -d ~/.config/tmux/ ] && mkdir -p ~/.config/tmux/
 ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
